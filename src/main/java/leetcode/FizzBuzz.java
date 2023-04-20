e22ae2b9b316c856b7694250ecdae64957f61471
strdos <81235866+strdos@users.noreply.github.com>
2023-04-20 22:06:57 +0200

    FizzBuzz list

---
+++ src/main/java/leetcode/FizzBuzz.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(5));
+    }
+
+    public static List<String> fizzBuzz(int n) {
+        //iterate until n and add items to the list as per the requirements
+        List<String> fizzBuzzList = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                fizzBuzzList.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                fizzBuzzList.add("Fizz");
+            } else if (i % 5 == 0) {
+                fizzBuzzList.add("Buzz");
+            } else {
+                fizzBuzzList.add(String.valueOf(i));
+            }
+        }
+        return fizzBuzzList;
+    }
+}
+
+/* Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+
+Constraints:
+1 <= n <= 10(4)
+
+ */
