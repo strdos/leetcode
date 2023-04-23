ff4616f58427616415d52d23d21fa74afda08620
strdos <81235866+strdos@users.noreply.github.com>
2023-04-23 21:18:24 +0200

    RemoveElement

---
+++ src/main/java/leetcode/RemoveElement.java
@@ -0,0 +1,57 @@
+package leetcode;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        System.out.print(removeElement(new int[] {2,2,3}, 2));
+    }
+    public static int removeElementOld(int[] nums, int val) {
+        if (nums.length == 0) return 0;
+        int counter = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                counter++;
+            }
+        }
+        int temp = 0;
+        if (counter < nums.length / 2) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] == val) {
+                    temp = nums[i];
+                    nums[i] = nums[i+1];
+                    nums[i+1] = temp;
+                }
+            }
+            for (int i = 0; i < nums.length; i++) {
+                System.out.print(nums[i] + " ");
+            }
+        }
+        return nums.length - counter;
+    }
+    public static int removeElement(int[] nums, int val) {
+        if (nums.length == 0) return 0;
+
+        int counter = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                counter++;
+            }
+        }
+
+        int temp = 0;
+        for (int i = 0; i < nums.length / 2 + 1; i++) {
+            for (int j = 0; j < nums.length - 1; j++) {
+                if (nums[j] == val) {
+                    temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+
+            for (int i = 0; i < nums.length; i++) {
+                System.out.print(nums[i] + " ");
+            }
+            System.out.println();
+        return nums.length - counter;
+    }
+}
