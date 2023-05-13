f6807f997dbc125b89487567695b67d921ae8bf0
strdos <81235866+strdos@users.noreply.github.com>
2023-05-13 13:54:51 +0200

    ContainerWithMostWater

---
+++ src/main/java/leetcode/ContainerWithMostWater.java
@@ -0,0 +1,73 @@
+package leetcode;
+
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        int[] height = new int[] {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height));
+    }
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+        int curArea;
+        int minHeight;
+
+        while (left < right) {
+            minHeight = Math.min(height[left], height[right]);
+            curArea = minHeight * (right - left);
+            maxArea = Math.max(maxArea, curArea);
+            if (height[left] < height[right]) {
+                left++;
+            } else if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return maxArea;
+    }
+    public static int solution(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int max = 0;
+        while(left < right){
+            int w = right - left;
+            int h = Math.min(height[left], height[right]);
+            int area = h * w;
+            max = Math.max(max, area);
+            if(height[left] < height[right]) left++;
+            else if(height[left] > height[right]) right--;
+            else {
+                left++;
+                right--;
+            }
+        }
+        return max;
+    }
+}
+
+/*
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+
+
+Constraints:
+
+n == height.length
+2 <= n <= 105
+0 <= height[i] <= 104
+ */
